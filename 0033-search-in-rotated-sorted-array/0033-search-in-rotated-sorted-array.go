4d72441aa72600ea1ca7c045043b11d84d6ebd43
Aditya Satapathy <satgriezeleo1007@gmail.com>
2026-07-05 13:19:53 +0530

    Time: 0 ms (100%), Space: 4.4 MB (20.67%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.go
@@ -0,0 +1,37 @@
+func search(nums []int, target int) int {
+    n:=len(nums)
+    low:=0
+    high:=n-1
+
+    for low<=high{
+        guess:=(low+high)/2
+
+        if nums[guess]==target{
+            return guess
+        }
+
+        if nums[guess]>nums[n-1]{
+            if nums[guess]<target{
+                low=guess+1
+            }else{
+                if nums[0]>target{
+                    low=guess+1
+                }else{
+                    high=guess-1
+                }
+            }
+            continue
+        }
+
+        if nums[guess]>target{
+            high=guess-1
+        }else{
+            if nums[n-1]<target{
+                high=guess-1
+            }else{
+                low=guess+1
+            }
+        }
+    }
+    return -1
+}
