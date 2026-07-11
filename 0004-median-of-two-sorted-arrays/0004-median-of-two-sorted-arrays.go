1dbdc9020a3fa6b6b288f9aa01bcc8c8d7541053
Aditya Satapathy <satgriezeleo1007@gmail.com>
2026-07-11 23:25:12 +0530

    Time: 0 ms (100%), Space: 6.5 MB (94.52%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.go
@@ -0,0 +1,52 @@
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+    n:=len(nums1)
+    m:=len(nums2)
+
+    if n>m{
+        return findMedianSortedArrays(nums2,nums1)
+    }
+
+    low:=0
+    high:=n
+
+    for low<=high{
+        cut1:=(low+high)/2
+        cut2:=(n+m+1)/2-cut1
+
+        left1:=math.MinInt
+        if cut1!=0{
+            left1=nums1[cut1-1]
+        }
+
+        left2:=math.MinInt
+        if cut2!=0{
+            left2=nums2[cut2-1]
+        }
+
+        right1:=math.MaxInt
+        if cut1!=n{
+            right1=nums1[cut1]
+        }
+
+        right2:=math.MaxInt
+        if cut2!=m{
+            right2=nums2[cut2]
+        }
+
+        if left1<=right2 && left2<=right1{
+            if (n+m)%2==0{
+                return float64(max(left1,left2)+min(right1,right2))/2.0
+            }else{
+                return float64(max(left1,left2))
+            }
+        }
+
+        if left1>right2{
+            high=cut1-1
+        }else{
+            low=cut1+1
+        }
+    }
+
+    return 0.0
+}
