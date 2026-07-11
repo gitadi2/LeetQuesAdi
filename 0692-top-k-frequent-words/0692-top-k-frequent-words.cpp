53c044ecaf405e282421bee941ca1ea7d91c497e
Aditya Satapathy <satgriezeleo1007@gmail.com>
2026-07-11 12:15:36 +0530

    Time: 3 ms (62.37%), Space: 18.3 MB (24.15%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -1,42 +1,46 @@
 class Solution {
 public:
-    struct cmp{
-        bool operator()(const pair<int,string>&a,const pair<int,string>&b){
-            if(a.first!=b.first){
-                return a.first>b.first;
-            }
-            return a.second<b.second;
+struct cmp{
+    bool operator()(const pair<int,string>&a, const pair<int,string>&b){
+        if(a.first!=b.first){
+            return a.first>b.first;         // Min Heap of the First 
         }
-    };
+        return b.second>a.second;           // Max heap on the Second one 
+    }
+};
 
     vector<string> topKFrequent(vector<string>& words, int k) {
+        int n=words.size();
+        
+        // Priority Queue Declaration
+        priority_queue<pair<int,string>,vector<pair<int,string>>,cmp> pq; 
+
+        // Hashmap Declaration
         unordered_map<string,int> mp;
 
-        for(auto word:words){
-            mp[word]++;                          // Hashmap mein frequency store karo
-        }
-
-        priority_queue<pair<int,string>,vector<pair<int,string>>,cmp> pq;
+        // Loop for the Hashmap
+        for(int i=0;i<n;i++){
+            mp[words[i]]++;
+        }  
 
         for(auto i:mp){
-            string word=i.first;
+            string words=i.first;
             int freq=i.second;
 
-            pair<int,string> curr={freq,word};
+            pair<int,string> curr={freq,words};
 
             if(pq.size()<k){
-                pq.push(curr);                   // Phele k elements daal do
+                pq.push(curr);
                 continue;
             }
 
             if(curr.first<pq.top().first){
-                continue;                        // Frequency chhoti hai toh skip
+                continue;
             }
 
             if(curr.first==pq.top().first && curr.second>pq.top().second){
-                continue;                        // Lexicographically bada hai toh skip
+                continue;
             }
-
             pq.pop();
             pq.push(curr);
         }
@@ -50,6 +54,6 @@ public:
 
         reverse(res.begin(),res.end());
 
-        return res;
+        return res; 
     }
 };
