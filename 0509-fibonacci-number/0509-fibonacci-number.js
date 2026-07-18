13dabfa6cf36bf16bafb69d332d0ae7e8e857f5f
Aditya Satapathy <satgriezeleo1007@gmail.com>
2026-07-18 11:12:49 +0530

    Time: 60 ms (48.64%), Space: 52.6 MB (95.15%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function(n) {
+    if(n==0){
+        return 0;
+    }
+    if(n==1){
+        return 1;
+    }
+
+    // Recursive call karlo 
+    let ans=fib(n-1)+fib(n-2);
+
+    return ans ;
+};
