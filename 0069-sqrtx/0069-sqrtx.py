6001300b33897e4cac223d32df96ec17705acb00
Aditya Satapathy <satgriezeleo1007@gmail.com>
2026-07-20 22:38:50 +0530

    Time: 3 ms (65.94%), Space: 19.3 MB (23.49%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,33 @@
+class Solution:
+
+    def mySqrt(self,x):
+
+        # agar x 0 ya 1 hai
+        if x<=1:
+            return x
+
+        l=1
+        h=x
+        ans=0
+
+        # binary search chalao
+        while l<=h:
+
+            m=l+(h-l)//2
+
+            # agar square root mil gaya
+            if m==x//m:
+                return m
+
+            # agar square chota hai
+            if m<x//m:
+
+                ans=m
+                l=m+1
+
+            # warna left mei jao
+            else:
+                h=m-1
+
+        # answer return kardo
+        return ans
