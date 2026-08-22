75a9d572a89683fcad7767d6ab5c5cbde7eedf2c
Aditya Satapathy <satgriezeleo1007@gmail.com>
2026-08-22 23:19:57 +0530

    Time: 0 ms (100%), Space: 3.9 MB (95.24%) - LeetHub

---
+++ 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.go
@@ -0,0 +1,15 @@
+func checkDivisibility(n int) bool {
+    sum:=0
+    product:=1
+    x:=n
+
+    // Har digit ko nikalenge
+    for x!=0{
+        digit:=x%10
+        x/=10
+        sum+=digit
+        product*=digit
+    }
+    // Divisibility check
+    return n%(sum+product)==0
+}
