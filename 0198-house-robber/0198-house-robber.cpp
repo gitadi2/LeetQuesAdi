49231e118961e3a162777fe102ecd6294bf1bd07
Aditya Satapathy <satgriezeleo1007@gmail.com>
2026-08-27 12:10:53 +0530

    Time: 0 ms (100%), Space: 11 MB (6.15%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,31 +1,22 @@
 class Solution {
 public:
-int fun(vector<int>&nums,int n,int i,bool free,vector<vector<int>>&dp){
-    // base Case: Jab last ghar mein pahunch jayein 
-    if(i==n){
-        return 0;
-    }
-    // Agar Dp k 2d matrix mei ans hai 
-    if(dp[i][free]!=-1){
-        return dp[i][free];
-    }
-    if(free==0){
-        // Toh agey badh jao and next wley ka free will 1 kardo 
-        return dp[i][free]=fun(nums,n,i+1,1,dp);
-    }
-    // agar free 0 nhi hai toh 2 cases banega 
-    int c1=nums[i]+fun(nums,n,i+1,0,dp);            // Agar Rob karhey toh y case 1 
-    int c2=fun(nums,n,i+1,1,dp);                    // Agar Rob nhi Karhey hai y case 2 
-
-    return dp[i][free]=max(c1,c2);                  // Max wla jo hoga whi final return kardo 
-}
-
-    int rob(vector<int>& nums) {
+    int rob(vector<int>&nums) {
         int n=nums.size();
-        // matrix of DP bna Lo 
-        vector<vector<int>>dp(n,vector<int>(2,-1));            // n*2 size ka Dp with -1 in every cell 
+        // Matrix of DP bna lo
+        vector<vector<int>>dp(n+1,vector<int>(2));
 
-        // Recursive Function Call kardo 
-        return fun(nums,n,0,1,dp);
+        // Base Case: Jab last ghar mein pahunch jayein
+        dp[n][0]=0;
+        dp[n][1]=0;
+
+        // n-1 se 0 tak fill karo
+        for(int i=n-1;i>=0;i--){
+            // Agar free==0 hai toh current house ko skip karna padega
+            dp[i][0]=dp[i+1][1];
+
+            // Agar free==1 hai, toh 2 cases banenge
+           dp[i][1]=max(nums[i]+dp[i+1][0],dp[i+1][1]);
+        }
+        return dp[0][1];
     }
 };
