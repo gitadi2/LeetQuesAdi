c64e1c2672294084a86e698fcbbdf7998ac56025
Aditya Satapathy <satgriezeleo1007@gmail.com>
2026-08-31 18:11:42 +0530

    Time: 268 ms (35.09%), Space: 296.5 MB (13.71%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,33 +1,29 @@
-// LIS using Memoization Method in DP
+// By the Tabulation Method of DP
 class Solution {
 public:
-int fun(vector<int>&nums,int n,int i,int prevEle,vector<vector<int>>&dp){
-    // Base case: jab hum last element par reach kar gye in array 
-    if(i==n){
-        return 0;
-    }
-    // Agar DP wley matrix mein pheley se hi hai 
-    if(dp[i][prevEle+1]!=-1){
-        return dp[i][prevEle+1];
-    }
-
-    if(prevEle==-1 || nums[prevEle]<nums[i]){
-        // Agar prev element kuch hai hi nhi ya fir prev elemnt current element se chota hai , toh 2 choices
-        int yes=1+fun(nums,n,i+1,i,dp);       // Agar humney lia 
-        int no=fun(nums,n,i+1,prevEle,dp);     // Agar humney nhi lia 
-
-        return dp[i][prevEle+1]=max(yes,no);                // Max Ans from both seen paths dekhlo 
-    }
-    // Agar prevEle hai kch bhi and woh bda hai curr ele se , toh nhi hi lengey 
-    return dp[i][prevEle+1]=fun(nums,n,i+1,prevEle,dp);
-}
-
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
+        // 2D Array(Matrix) of the size n+1*n+1 bna lo 
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
 
-        // 2D arry(matrix) bna lo of size n+1*n+1
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        // Recursive Function Call kardo 
-        return fun(nums,n,0,-1,dp);
+        // Base Case: Nth Row of the Dp Matrix bhar lo 
+        for(int j=0;j<=n;j++){
+            dp[n][j]=0;                 // Pura Nth Row 0 se bhar do 
+        }
+        // Ab pheley k Rows Bharo 
+        for(int i=n-1;i>=0;i--){
+            for(int prevEle=-1;prevEle<=i-1;prevEle++){
+                // Agar PrevEle kuch nhi hai ya fir PrevEle chota hai than curr element
+                if(prevEle==-1 || nums[prevEle]<nums[i]){
+                    // 2 Choice banega uska max lelo 
+                    dp[i][prevEle+1]=max(1+dp[i+1][i+1],dp[i+1][prevEle+1]);
+                }
+                // Agar esa hi hai
+                else{
+                    dp[i][prevEle+1]=dp[i+1][prevEle+1];          // Ek hi choice hai as No toh agey badhado
+                }
+            }
+        }
+        return dp[0][0];                // Final ans return kardo
     }
 };
