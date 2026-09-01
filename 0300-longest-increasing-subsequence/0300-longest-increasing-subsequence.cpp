c438cae5c7862fecae9f0010708352363ed712a6
Aditya Satapathy <satgriezeleo1007@gmail.com>
2026-09-01 21:41:18 +0530

    Time: 0 ms (100%), Space: 14.1 MB (87.29%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,29 +1,44 @@
-// By DP Approach  (Time Complexity O[n^2])
+// By Binary Search Approach {THE MOST OPTIMAL APPROACH}
 
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
+    int lengthOfLIS(vector<int>&nums) {
         int n=nums.size();
+        // Ek Resultant ans array bna lo
+        vector<int>res;
 
-        // Ek Resultant ans arry bna lo of size n
-        vector<int>res(n);   
-        int i,j;
+        for(int i=0;i<n;i++){
+            // Current element ko lo
+            int x=nums[i];
 
-        for(i=0;i<n;i++){
-            // Sarey elements ko lo uskey liye loop
-            res[i]=1;                              // Har kisika LIS 1 toh hoga hi  
-            for(j=0;j<i;j++){
-                if(nums[j]<nums[i]){
-                    // agr previous element chota hai than current element tabhi lena hai 
-                    res[i]=max(res[i],res[j]+1);            // max lelo par previous LIS mein 1 add karke
-                } 
+            // Binary Search se first element >= x dhundho
+            int left=0;
+            int right=res.size();
+
+            while(left<right){
+                int mid=left+(right-left)/2;
+                if(res[mid]<x){
+                    // Agar mid wala element chota hai
+                    // Toh right side mein jao
+                    left=mid+1;
+                }
+                else{
+                    // Agar mid wala element bada ya equal hai
+                    // Toh left side mein jao
+                    right=mid;
+                }
+            }
+            // Agar koi element >= x nhi mila
+            if(left==res.size()){
+                // Current element ko end mein daal do
+                res.push_back(x);
+            }
+            else{
+                // Smallest possible ending value update karo
+                res[left]=x;
             }
         }
-        int ans=1;                  // By Default 1 toh rahega hi 
-
-        for(i=0;i<n;i++){
-            ans=max(ans,res[i]);        // purey ans se jo max nikla whi final 
-        }
-        return ans;
+        // res ka size hi LIS ki length hai
+        return res.size();
     }
 };
