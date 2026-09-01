ace0f1a2e66395c6dba6fbf4afa95ba6e0c5b818
Aditya Satapathy <satgriezeleo1007@gmail.com>
2026-09-01 21:33:13 +0530

    Time: 79 ms (46.72%), Space: 14.5 MB (41.11%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,29 +1,29 @@
-// By the Tabulation Method of DP
+// By DP Approach  (Time Complexity O[n^2])
+
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        // 2D Array(Matrix) of the size n+1*n+1 bna lo 
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
 
-        // Base Case: Nth Row of the Dp Matrix bhar lo 
-        for(int j=0;j<=n;j++){
-            dp[n][j]=0;                 // Pura Nth Row 0 se bhar do 
-        }
-        // Ab pheley k Rows Bharo 
-        for(int i=n-1;i>=0;i--){
-            for(int prevEle=-1;prevEle<=i-1;prevEle++){
-                // Agar PrevEle kuch nhi hai ya fir PrevEle chota hai than curr element
-                if(prevEle==-1 || nums[prevEle]<nums[i]){
-                    // 2 Choice banega uska max lelo 
-                    dp[i][prevEle+1]=max(1+dp[i+1][i+1],dp[i+1][prevEle+1]);
-                }
-                // Agar esa hi hai
-                else{
-                    dp[i][prevEle+1]=dp[i+1][prevEle+1];          // Ek hi choice hai as No toh agey badhado
-                }
+        // Ek Resultant ans arry bna lo of size n
+        vector<int>res(n);   
+        int i,j;
+
+        for(i=0;i<n;i++){
+            // Sarey elements ko lo uskey liye loop
+            res[i]=1;                              // Har kisika LIS 1 toh hoga hi  
+            for(j=0;j<i;j++){
+                if(nums[j]<nums[i]){
+                    // agr previous element chota hai than current element tabhi lena hai 
+                    res[i]=max(res[i],res[j]+1);            // max lelo par previous LIS mein 1 add karke
+                } 
             }
         }
-        return dp[0][0];                // Final ans return kardo
+        int ans=1;                  // By Default 1 toh rahega hi 
+
+        for(i=0;i<n;i++){
+            ans=max(ans,res[i]);        // purey ans se jo max nikla whi final 
+        }
+        return ans;
     }
 };
