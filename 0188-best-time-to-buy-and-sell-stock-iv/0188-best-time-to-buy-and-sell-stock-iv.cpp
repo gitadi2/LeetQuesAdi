383c48d161e6a041d9afc61b61e8e07d3de8348d
Aditya Satapathy <satgriezeleo1007@gmail.com>
2026-09-04 13:19:43 +0530

    Time: 0 ms (100%), Space: 17.6 MB (62.06%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,41 +1,38 @@
-// By the Recursive Method in DP
+// By the Tabulation Method in the DP
 
 class Solution {
 public:
-int fun(vector<int>&prices,int n,int i,int k,vector<vector<int>>&dp){
-    // base Case: jab last din mein reach kar gye 
-    if(i==n){
-        return 0;
-    }
-    // Agar no. of trans hai hi nhi 
-    if(k==0){
-        return 0;
-    }
-    // Agar pheley se dp k matrix mei hai val toh return kardo 
-    if(dp[i][k]!=-1){
-        // Yni ki present hai 
-        return dp[i][k];
-    }
-    if(k%2==0){
-        // agr Even hai: Toh Buy ka Case banado 
-        int c1=fun(prices,n,i+1,k-1,dp)-prices[i];            // Buy kardia 
-        int c2=fun(prices,n,i+1,k,dp);                        // Simply move fwd without Buying 
-        return dp[i][k]=max(c1,c2);
-    }
-    else{
-        // Agar Odd hai: Toh Sell wla Case banega 
-        int c1=fun(prices,n,i+1,k-1,dp)+prices[i];           // Sell kardia 
-        int c2=fun(prices,n,i+1,k,dp);                       // Simply move fwd without selling 
-        return dp[i][k]=max(c1,c2);
-    }
-}
-
     int maxProfit(int k, vector<int>& prices) {
         int n=prices.size();
-        int transactions=k*2;                                         // Atmost k Times hai 
-        // 2D array(matrix) of DP bna lo of size n+1*k+1
-        vector<vector<int>>dp(n+1,vector<int>(transactions+1,-1));
-        int profit=fun(prices,n,0,transactions,dp);                    // Recursive Function call kardo 
-        return profit;
+        int transactions=k*2;                        // Atmost k No. of the Transactions hai 
+        // 2D array(matrix) of Dp bna lo of size n+1*tarnsactions+1
+        vector<vector<int>>dp(n+1,vector<int>(transactions+1));
+
+        // Base Case : Jab last Din mein reach hojae 
+        for(int j=0;j<=transactions;j++){
+            dp[n][j]=0;                             // Nth Row ko comopletely 0 se fill kardo 
+        }
+        // Agar kuch trans ho hi nhi rha hai 
+        for(int i=0;i<=n;i++){
+            dp[i][0]=0;                            
+        }
+        // Ab Reverse order mein bharna shuru kardo 
+        for(int i=n-1;i>=0;i--){
+            for(int j=1;j<=transactions;j++){
+                // Buy wla case banega 
+                if(j%2==0){
+                    int c1=dp[i+1][j-1]-prices[i];                   // Buy karlia 
+                    int c2=dp[i+1][j];                               // agey badho without buying 
+                    dp[i][j]=max(c1,c2);
+                }
+                else{
+                    // Agar Odd wla hai, toh sell banega : if(j%2!=0)
+                    int c1=dp[i+1][j-1]+prices[i];                   // Sell kardia 
+                    int c2=dp[i+1][j];                               // Agey badho without selling 
+                    dp[i][j]=max(c1,c2);
+                }
+            }
+        }
+        return dp[0][transactions];                              // Final Max. Profit return kardo 
     }
 };
