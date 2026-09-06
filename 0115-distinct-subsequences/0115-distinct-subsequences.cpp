a4ee2f81c2a13fb668cea9d6d5dcd4bc92a7c5a4
Aditya Satapathy <satgriezeleo1007@gmail.com>
2026-09-06 10:23:37 +0530

    Time: 31 ms (53.36%), Space: 44.4 MB (11.48%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -0,0 +1,32 @@
+// By 2D DP Tabulation Method
+
+class Solution {
+public:
+    int numDistinct(string s,string t) {
+        int n=s.size();
+        int m=t.size();
+
+        // dp[i][j] = s ke first i characters se t ke first j characters banane ke total number of ways
+        vector<vector<unsigned long long>>dp(n+1,vector<unsigned long long>(m+1,0));
+
+        // Empty t ko kisi bhi s se banane ka 1 way hai
+        for(int i=0;i<=n;i++){
+            dp[i][0]=1;
+        }
+
+        // DP table fill karenge
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1]==t[j-1]){
+                    // Current character ko lena + skip karna
+                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                }
+                else{
+                    // Current character match nahi karta, isliye skip
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][m];
+    }
+};
