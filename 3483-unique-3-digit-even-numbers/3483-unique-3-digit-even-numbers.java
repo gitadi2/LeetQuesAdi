f63fe9b1ae9d2ea6203b77348287e36968201d36
Aditya Satapathy <satgriezeleo1007@gmail.com>
2026-09-11 09:38:24 +0530

    Time: 1 ms (100%), Space: 43.5 MB (99.73%) - LeetHub

---
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int totalNumbers(int[] digits) {
+        int n=digits.length;
+        int[] mp=new int[10];                  // Hash Table bna lo
+
+        // Har digit ki frequency store karo
+        for(int i=0;i<n;i++){
+            mp[digits[i]]++;
+        }
+        int res=0;                            // Answer ko 0 se shuru karo
+
+        // 3 digit number ke liye first digit 1-9 hona chahiye
+        for(int i=1;i<=9;i++){
+            if(mp[i]==0){
+                continue;
+            }
+            mp[i]--;                          // First digit use kardo
+
+            // Middle digit 0-9 kuch bhi ho sakta hai
+            for(int j=0;j<=9;j++){
+                if(mp[j]==0){
+                    continue;
+                }
+                mp[j]--;                      // Middle digit use kardo
+
+                // Last digit even hona chahiye
+                for(int k=0;k<=8;k+=2){
+                    if(mp[k]>0){
+                        res++;                // Ek valid number mil gaya
+                    }
+                }
+                mp[j]++;                      // Middle digit wapas daaldo
+            }
+            mp[i]++;                          // First digit wapas daaldo
+        }
+        return res;
+    }
+}
