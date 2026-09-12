927877f6c5a1bfb1db0a46d78d6c3b6e76046c70
Aditya Satapathy <satgriezeleo1007@gmail.com>
2026-09-12 14:07:06 +0530

    Time: 679 ms (12.16%), Space: 0B (100%) - LeetHub

---
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+
+SELECT today.id 
+FROM Weather
+AS today
+WHERE EXISTS
+    (SELECT 1 FROM Weather AS yesterday
+        WHERE yesterday.temperature<today.temperature
+        AND
+        DATEDIFF(today.recordDate,yesterday.recordDate)=1
+    )
