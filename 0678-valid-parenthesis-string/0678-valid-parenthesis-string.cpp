08c77866a81734aa12ceb008fa2afe21d48e7638
Aditya Satapathy <satgriezeleo1007@gmail.com>
2026-09-24 14:07:32 +0530

    Time: 0 ms (100%), Space: 8.2 MB (47.55%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,50 +1,35 @@
-// Tabulation Approach
+// Range Based Approach 
 
 class Solution {
 public:
-    bool checkValidString(string s){
+    bool checkValidString(string s) {
         int n=s.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        int min=0;            // Min of the Range 
+        int max=0;           // Max of the Range 
 
-        // Base case
-        dp[n][0]=1;
-
-        for(int ind=n-1;ind>=0;ind--){
-            for(int cnt=0;cnt<=n;cnt++){
-                // Agar Opening Parenthesis mila '('
-                if(s[ind]=='('){
-                    if(cnt+1<=n){
-                        dp[ind][cnt]=dp[ind+1][cnt+1];
-                    }
-                }
-
-                // Agar Closing hua toh ')'
-                else if(s[ind]==')'){
-                    if(cnt>0){
-                        dp[ind][cnt]=dp[ind+1][cnt-1];
-                    }
-                }
-
-                // Agar Asterick '*' mila toh
-                else if(s[ind]=='*'){
-                    // '*' ko '(' maan lo
-                    bool c1=false;
-                    if(cnt+1<=n){
-                        c1=dp[ind+1][cnt+1];
-                    }
-
-                    // '*' ko ')' maan lo
-                    bool c2=false;
-                    if(cnt>0){
-                        c2=dp[ind+1][cnt-1];
-                    }
-
-                    // '*' ko empty maan lo
-                    bool c3=dp[ind+1][cnt];
-                    dp[ind][cnt]=c1||c2||c3;
-                }
+        for(int i=0;i<n;i++){
+            //Agar '(' mila 
+            if(s[i]=='('){
+                min++;
+                max++;
+            }
+            //Agar ')' mila 
+            else if(s[i]==')'){
+                min--;
+                max--;
+            }
+            // Agar '*' mila 
+            else{
+                min--;
+                max++;
+            }
+            if(min<0){
+                min=0;          // agr min -ve hua toh update it to zero 
+            }
+            if(max<0){
+                return false;
             }
         }
-        return dp[0][0];
+        return min==0;               // Yni ki possible valid parenthesis hai toh whi
     }
 };
